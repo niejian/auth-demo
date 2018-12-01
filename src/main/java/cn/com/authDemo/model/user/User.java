0f523fb7fb230c9514026bcba579802daf68dda0
niejian <niejian9001@163.com>
2018-12-01 21:54:19 +0800

    天剑测试用例

--- src/main/java/cn/com/authDemo/model/user/User.java
+++ src/main/java/cn/com/authDemo/model/user/User.java
@@ -3,12 +3,26 @@ package cn.com.authDemo.model.user;/**
  */
 
 import lombok.Data;
+import lombok.ToString;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+import org.springframework.data.mongodb.core.mapping.Field;
 
 /**
+ * 定义实体
  * @author niejian
  * @date 2018/12/1
  */
 @Data
-
+@ToString
+@Document(collection = "user")
 public class User {
+
+    @Id
+    @Field("userId")
+    private String id;
+    @Field("userName")
+    private String userName;
+    @Field("userCode")
+    private String userCode;
 }
