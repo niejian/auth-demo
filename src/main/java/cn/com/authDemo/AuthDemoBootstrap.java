926c106977710e5f7559399a6e0dfbbdd58e8e22
niejian <niejian@bluemoon.com.cn>
2018-12-08 11:02:35 +0800

    添加运行第三方tomcat配置

--- src/main/java/cn/com/authDemo/AuthDemoBootstrap.java
+++ src/main/java/cn/com/authDemo/AuthDemoBootstrap.java
@@ -2,15 +2,36 @@ package cn.com.authDemo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.ServletContextApplicationContextInitializer;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.web.context.ConfigurableWebApplicationContext;
 
 /**
  * @author: nj
  * @date: 2018/11/26:下午5:31
+ * 继承SpringBootServletInitializer部署到独立的tomcat中
  */
 @EnableAsync
 @SpringBootApplication
-public class AuthDemoBootstrap {
+public class AuthDemoBootstrap extends SpringBootServletInitializer {
+
+    /**
+     * Configure the application. Normally all you would need to do is to add sources
+     * (e.g. config classes) because other settings have sensible defaults. You might
+     * choose (for instance) to add default command line arguments, or set an active
+     * Spring profile.
+     *
+     * @param builder a builder for the application context
+     * @return the application builder
+     * @see SpringApplicationBuilder
+     */
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(AuthDemoBootstrap.class);
+    }
+
     public static void main(String[] args) {
         SpringApplication.run(AuthDemoBootstrap.class);
     }
